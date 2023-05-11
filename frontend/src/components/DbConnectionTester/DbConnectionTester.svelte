5d9af007b20fc75bd5a0f732c66aa74e0d6fe2cd
Kristian <kristian.dam.pedersen@gmail.com>
2023-05-11 12:01:55 +0200

    Kristians stuff

---
+++ frontend/src/components/DbConnectionTester/DbConnectionTester.svelte
@@ -0,0 +1,11 @@
+<script>
+// Make a HTTP Request upon clicking a button
+async function testConnection() {
+    const response = await fetch('http://localhost:5000/postgres');
+    const data = await response.json();
+    console.log(data);
+}
+</script>
+
+<button on:click={testConnection}>Test Connection</button>
+
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import DbConnectionTester from "../components/DbConnectionTester/DbConnectionTester.svelte";
+</script>
+<h1>Velkommen til projektet bois</h1>
+<DbConnectionTester />
