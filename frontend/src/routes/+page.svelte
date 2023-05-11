6935ace9510a97767adf6aa2eb3a7edeee5a27f0
Kristian <kristian.dam.pedersen@gmail.com>
2023-05-11 16:02:49 +0200

    started working on the website

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script>
     import DbConnectionTester from "../components/DbConnectionTester/DbConnectionTester.svelte";
+    import Navbar from "../components/Navbar/Navbar.svelte";
+    import theme from "../theme.js"
 </script>
-<h1>Velkommen til projektet bois</h1>
-<DbConnectionTester />
+<div class="main-container">
+  <Navbar />
+  <h1 style="--theme: {theme.colors.kuGray}">Velkommen til projektet bois</h1>
+  <DbConnectionTester />
+</div>
+<style scoped>
+  :global(body) {
+    margin: 0
+  }
+  h1 {
+    color: var(--theme)
+  }
+
+  .main-container {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
---
+++ frontend/src/theme.js
@@ -0,0 +1,12 @@
+let theme = {
+  "colors": {
+    "kuRed": "#901a1e",
+    "kuGray": "#14314f",
+    "darkGray": "#2b2d41",
+    "dulledWhite": "#f4f5f7",
+    "greyedOut": "#8d99ad"
+  }
+}
+
+export default theme
+
