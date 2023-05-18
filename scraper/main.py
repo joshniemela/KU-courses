87c8c0ccb7e6317515212f53735cf2ce00753b89
Joshua Niemelä <josh@jniemela.dk>
2023-05-18 15:06:15 +0200

    Made the scraper parallel

--- scraper/main.py
+++ scraper/main.py
@@ -1,20 +1,24 @@
 from parsing import get_all_info
 from scraper import get_sitemap_urls, cache_pages
 import json
+import multiprocessing as mp
 
+def convert_to_json(url:str):
+    """
+    Converts all the html files in ../data/html to json files in ../data/json
+    """
+    try:
+        with open(f"../data/json/{url.split('/')[-1]}.json", "w") as f:
+            json.dump(get_all_info(url), f)
+            print(f"Parsed {url}")
+    except Exception as e:
+        print(f"Error with {url}")
+        print(e)
+        return
+
+    
 if __name__=="__main__":
     cache_pages()
     sitemap_urls = get_sitemap_urls()
-    
-    # TODO: multithread this
-    for url in sitemap_urls:
-        try:
-            # write to json
-            with open(f"../data/json/{url.split('/')[-1]}.json", "w") as f:
-                json.dump(get_all_info(url), f)
-                print(f"Parsed {url}")
-        except Exception as e:
-            print(f"Error with {url}")
-            print(e)
-            continue
-
+    with mp.Pool(8) as p:
+        p.map(convert_to_json, sitemap_urls)
