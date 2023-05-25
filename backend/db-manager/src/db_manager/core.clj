c4ae49f9e0412a132e04cb7d917e2cb3f351a51f
Joshua Niemelä <josh@jniemela.dk>
2023-05-25 21:27:36 +0200

    Added employee import

--- backend/db-manager/src/db_manager/core.clj
+++ backend/db-manager/src/db_manager/core.clj
@@ -1,19 +1,19 @@
-(ns db-manager.core
-  (:refer-clojure :exclude [filter for group-by into partition-by set update])
-  (:require [next.jdbc :as jdbc] 
-            [honey.sql :as sql] 
-            [honey.sql.helpers :refer :all :as h] 
-            [clojure.core :as c]
+(ns db-manager.core 
+  (:refer-clojure :exclude [filter for into partition-by set update])
+  (:require [clojure.core :as c]
             [clojure.data.json :as json]
             [clojure.java.io :as io]
             [muuntaja.core :as m]
-            [reitit.ring :as ring] 
+            [reitit.ring :as ring]
             [reitit.coercion.spec]
             [reitit.ring.coercion :as rrc]
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.parameters :as parameters]
             [org.httpkit.server :refer [run-server]]
-            [db-manager.routes :refer [ping-route crud-routes]]))
+            [db-manager.routes :refer [ping-route crud-routes]]
+            [db-manager.db :refer [nuke-replace-employees!]]
+            [next.jdbc :as jdbc]
+            [honey.sql :as sql]))
                                              
 
 (def db-config
@@ -25,24 +25,9 @@
 
 (def db (jdbc/get-datasource db-config))
 
-(defn init-table! [table-name columns]
-  (jdbc/execute! db (sql/format {:create-table [table-name :if-not-exists]
-                                 :with-columns columns})))
-
-(defn init-tables! [tables]
-  (map #(init-table! (first %) (second %)) tables))
-
 ; read employed.json
 (def employees (json/read-str (slurp "employed.json") :key-fn keyword))
 
-; upsert employees
-(defn upsert-employees [employees]
-  (-> (insert-into :employees)
-      ; values are the keys of the table
-      (values (map #(select-keys % [:name :email :title]) employees))
-      (on-conflict :email)
-      do-nothing
-      sql/format))
 
 (defn app []
   (ring/ring-handler
@@ -64,8 +49,21 @@
   (run-server (app) {:port 3000})
 
 (comment 
-  (init-tables! [employee-table course-table])
-  (jdbc/execute! db ["drop table employees;"])
-  (upsert-employees (take 10 employees)))
-  (jdbc/execute! db (upsert-employees employees))
+  ; some employees have multiple titles, so we need to group them
+  (defn merge-employees [employees] 
+    (let [grouped (group-by :email employees)]
+      (map (fn [[email employees]] 
+             (reduce (fn [acc employee] 
+                       (assoc acc :title (str (:title acc) ", " (:title employee)))) 
+                     (first employees) 
+                     (rest employees))) 
+           grouped))) 
+  
+  (println (merge-employees employees))
+  (println (count employees))
+  (println (count (merge-employees employees))
+  (nuke-replace-employees! db (merge-employees employees)))
+  ; find person with email back@di.ku.dk
+  (println (jdbc/execute! db ["select * from employees;"])) 
+  (println (jdbc/execute! db ["select * from employees where email = 'back@di.ku.dk';"]))
 )
--- backend/db-manager/src/db_manager/db.clj
+++ backend/db-manager/src/db_manager/db.clj
@@ -1,18 +1,43 @@
-(ns db
-  )
+(ns db-manager.db 
+  (:refer-clojure :exclude [filter for group-by into partition-by set update])
+  (:require [next.jdbc :as jdbc]
+            [honey.sql :as sql]
+            [honey.sql.helpers :refer :all :as h]))
+
+
+(def employee-schema
+  [[:name [:varchar 255] [:not nil]] 
+   [:email [:varchar 50] [:not nil]] 
+   [:title [:varchar 255] [:not nil]]
+  ;[:phone [:varchar 255]] ;might not be allowed by GDPR 
+   [[:primary-key :email]]])
+
+(def course-schema
+  [[:course-id [:char 10] [:not nil]] 
+   [:placement [:char 3] [:not nil]]])
+
+; takes map of shape:
+; {:name "Erik" :email "foo@bar.dk" :title "CEO"}
+(defn insert-employees [employees]
+  (-> (insert-into :employees)
+      ; values are the keys of the table
+      (values (map #(select-keys % [:name :email :title]) employees))
+      sql/format))
 
 
 
+(defn nuke-replace-employees! [db employees]
+  ; drop employees table if exists,
+  ; then create it again
+  ; then insert employees
+  (jdbc/with-transaction [tx db]
+    (jdbc/execute! tx (sql/format {:drop-table [:if-exists :employees]}))
+    (jdbc/execute! tx (sql/format {:create-table [:employees :if-not-exists]
+                                   :with-columns employee-schema}))
+    (jdbc/execute! tx (insert-employees employees))))
+  
+  
 
-(def employee-table
-  [:employees [[:name [:varchar 255] [:not nil]]
-               [:email [:varchar 50] [:not nil]]
-               [:title [:varchar 255] [:not nil]]
-               [:phone [:varchar 255]] ;might not be allowed by GDPR
-               [[:primary-key :email]]]])
 
-(def course-table
-  [:courses [[:course-id [:char 10] [:not nil]]
-             [:placement [:char 3] [:not nil]]]])
 
 
