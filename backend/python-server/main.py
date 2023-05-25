bb5dea444e74ec1424dc447a4ef416b4aa1ff048
Kristian <kristian.dam.pedersen@gmail.com>
2023-05-25 11:38:57 +0200

    added basic python server

---
+++ backend/python-server/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return f'DISKU Python Server'
+
+@app.get("/courses")
+def read_courses():
+    return {"Hello": "From courses"}
+
+@app.get("/course_summaries/")
+def read_summaries():
+    return [
+            {
+                "course_name": "Diskret Matematik og Formelle Sprog",
+                "course_id": "1234abcd",
+                "summary": "Ultra short summary here",
+                "block": 1,
+                "schema_group": "A",
+                "institute": "Datalogisk Institut",
+                "faculty": "SCIENCE",
+                "level": "bachelor"
+            },
+            {
+                "course_name": "Diskret Matematik og Formelle Sprog",
+                "course_id": "1234abcd",
+                "summary": "Ultra short summary here",
+                "block": 1,
+                "schema_group": "A",
+                "institute": "Datalogisk Institut",
+                "faculty": "SCIENCE",
+                "level": "bachelor"
+            },
+            {
+                "course_name": "Diskret Matematik og Formelle Sprog",
+                "course_id": "1234abcd",
+                "summary": "Ultra short summary here",
+                "block": 1,
+                "schema_group": "A",
+                "institute": "Datalogisk Institut",
+                "faculty": "SCIENCE",
+                "level": "bachelor"
+            },
+            {
+                "course_name": "Diskret Matematik og Formelle Sprog",
+                "course_id": "1234abcd",
+                "summary": "Ultra short summary here",
+                "block": 1,
+                "schema_group": "A",
+                "institute": "Datalogisk Institut",
+                "faculty": "SCIENCE",
+                "level": "bachelor"
+            },
+            {
+                "course_name": "Diskret Matematik og Formelle Sprog",
+                "course_id": "1234abcd",
+                "summary": "Ultra short summary here",
+                "block": 1,
+                "schema_group": "A",
+                "institute": "Datalogisk Institut",
+                "faculty": "SCIENCE",
+                "level": "bachelor"
+            },
+            ]
