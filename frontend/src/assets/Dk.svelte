1a47945097b519ab23ab0402fc7a610e7a2dd96f
Kristian <kristian.dam.pedersen@gmail.com>
2023-06-03 13:20:08 +0200

    Rough sketch for overview cards

---
+++ frontend/src/assets/Dk.svelte
@@ -0,0 +1,15 @@
+<svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-dk" viewBox="0 0 640 480">
+  <path fill="#c8102e" d="M0 0h640.1v480H0z"/>
+  <path fill="#fff" d="M205.7 0h68.6v480h-68.6z"/>
+  <path fill="#fff" d="M0 205.7h640.1v68.6H0z"/>
+</svg>
+
+<style scoped>
+svg {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 10%;
+    border-top-left-radius: 20%;
+}
+</style>
--- frontend/src/routes/browse/+page.svelte
+++ frontend/src/routes/browse/+page.svelte
@@ -2,6 +2,7 @@
 import theme from "../../theme";
 import SearchIcon from "../../assets/SearchIcon.svelte";
 import FilterButton from "../../components/FilterButton/FilterButton.svelte";
+import Dk from "../../assets/Dk.svelte";
 
 </script>
 <div class="browse-container">
@@ -19,7 +20,34 @@ import FilterButton from "../../components/FilterButton/FilterButton.svelte";
                 --stagger: {0};
                 "
         >
+            <div class="card-header-container">
+                <div class="card-title-container">
+                    <h1 class="card-title" style="--text-color: {theme.colors.neutral[200]}">Course name</h1>
+                    <h2 class="card-subtitle" style="--text-color: {theme.colors.neutral[600]}">abcdefg-course-id - SCIENCE</h2>
+                </div>
+                <table class="card-info-table">
+                    <tr>
+                        <td class="card-td-left-top">Bachelor</td>
+                        <td class="card-td-right-top">15 ECTS</td>
+                    </tr>
+                    <tr>
+                        <td class="card-td-left-bot"> Block 4</td>
+                        <td> Group A</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="card-description-container">
+            <p class="card-description" style="--text-color: {theme.colors.neutral[200]}"> Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis. </p>
+            </div>
+            <div class="card-exam-text-container"
+                style="--bg-color: {theme.colors.neutral[300]}"
+            >
+                <p class="card-exam-text"
+                style="--text-color: {theme.colors.neutral[900]}"
 
+                >Løbende evaluering - Mundtlig Eksamen</p>
+            </div>
+            <Dk />
         </div>
         <div class="card"
             style="
@@ -27,7 +55,6 @@ import FilterButton from "../../components/FilterButton/FilterButton.svelte";
                 --stagger: {1};
                 "
         >
-
         </div>
         <div class="card"
             style="
@@ -85,6 +112,36 @@ import FilterButton from "../../components/FilterButton/FilterButton.svelte";
         >
 
         </div>
+        <div class="card"
+            style="
+                --bg-color: {theme.colors.neutral[800]};
+                --stagger: {6};
+                "
+        >
+
+        </div>
+        <div class="card"
+            style="
+                --bg-color: {theme.colors.neutral[800]};
+                --stagger: {7};
+                "
+        >
+
+        </div>
+        <div class="card"
+            style="
+                --bg-color: {theme.colors.neutral[800]};
+                --stagger: {8};
+                "
+        >
+        </div>
+        <div class="card"
+            style="
+                --bg-color: {theme.colors.neutral[800]};
+                --stagger: {8};
+                "
+        >
+        </div>
     </div>
 </div>
 
@@ -122,15 +179,21 @@ import FilterButton from "../../components/FilterButton/FilterButton.svelte";
 .card-container {
     height: 100%;
     width:96%;
-    margin: 2vh;
+    margin: 2%;
     display: grid;
-    grid-template: 1fr 1fr 1fr / 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-auto-rows: 33vh;
     justify-content: center;
     align-items: center;
     gap: 2vh;
 }
 
 .card {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
     height: 100%;
     width: 100%; 
     background-color: var(--bg-color);
@@ -140,11 +203,87 @@ import FilterButton from "../../components/FilterButton/FilterButton.svelte";
     opacity: 0%;
     animation: fadeIn 0.5s calc(var(--stagger)*0.05s);
     animation-fill-mode: forwards;
+    overflow: scroll;
 }
+
 .card:hover {
     scale: 1.02;
     box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.25);
     transition: ease-in-out 0.2s;
+}
+
+.card-header-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 96%;
+}
+
+.card-title-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+}
+
+
+.card-description-container {
+    width: 96%;
+    padding-left: 2%;
+    padding-right: 2%;
+    height: 80%;
+    overflow-y: scroll;
+
+}
+
+.card-exam-text-container {
+    position: relative;
+    background-color: var(--bg-color);
+    bottom: 0;
+    width: 100%;
+    height: 11.58%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-exam-text {
+    color: var(--text-color);
+}
+
+.card-title {
+    font-size: 2rem;
+    color: var(--text-color);
+}
+
+.card-subtitle {
+    font-size: 1rem;
+    color: var(--text-color);
+}
+
+.card-description {
+    font-size: 1rem;
+    color: var(--text-color);
+}
+
+.card-info-table {
+    width: 30%;
+    text-align: center;
+    border-spacing: 0;
+}
+
+.card-td-left-top {
+    border-bottom: 1px solid;
+    border-right: 1px solid;
+}
+
+.card-td-right-top {
+    border-bottom: 1px solid;
+}
+
+.card-td-left-bot {
+    border-right: 1px solid;
 }
 
 @keyframes fadeIn {
