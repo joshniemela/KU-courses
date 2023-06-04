acf931bfc17e40cb75a3e3b08b444201be882d11
Joshua Niemelä <josh@jniemela.dk>
2023-06-04 15:12:12 +0200

    Reformatted employed parser

--- backend/employed/main.py
+++ backend/employed/main.py
@@ -1,9 +1,9 @@
 from scraper import get_diku_staff
-import json    
+import json
 
 DATA_DIR = "../../data/"
 
-if __name__=="__main__":
+if __name__ == "__main__":
     staff = get_diku_staff()
     with open(DATA_DIR + "employed.json", "w") as outfile:
         outfile.write(staff.to_json(orient="records"))
