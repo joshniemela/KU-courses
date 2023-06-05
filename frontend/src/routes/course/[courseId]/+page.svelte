2296c142840edee9c68a3d643dedd38ab3ae1416
Kristian <kristian.dam.pedersen@gmail.com>
2023-06-05 12:30:25 +0200

    started styling the details page

--- frontend/src/routes/course/[courseId]/+page.svelte
+++ frontend/src/routes/course/[courseId]/+page.svelte
@@ -1,6 +1,9 @@
 <script>
 import { page } from '$app/stores';
 import { onMount } from 'svelte';
+import theme from '../../../theme';
+import Loader from '../../../components/Loader/Loader.svelte';
+import { LoremIpsum } from 'lorem-ipsum';
 import overview from '../../../mocking/overview.json';
 
 const courseId = $page.params.courseId;
@@ -9,6 +12,16 @@ let loading = true;
 
 let course = {};
 
+const desc = new LoremIpsum({
+    sentencesPerParagraph: {
+        max: 8,
+        min: 4
+    },
+    wordsPerSentence: {
+        max: 16,
+        min: 4
+    }
+});
 
 const matchId = (obj) => {
     console.log('matching id ' + obj.course_id)
@@ -30,26 +43,88 @@ onMount(async () => {
 
 </script>
 {#if loading}
-    <p>loading</p>
+    <Loader />
 {:else}
     <div class="main-container">
-        <div class="header-container">
-            <h1>{course.primary_title}</h1>
-            <h2>{course.course_id}</h2>
-        </div>
         <div class="content-container">
             <div class="content-container-left">
-                <p>Description</p>
+                <div class="header-container">
+                    <a href="/browse">
+                        <p>Go back</p>
+                    </a>
+                    <div>
+                        <h1>{course.primary_title}</h1>
+                        <h2>{course.course_id} - SCIENCE </h2>
+                    </div>
+                </div>
+                <h3>Description</h3>
+                <p>{desc.generateParagraphs(1)}</p>
+                <p>{desc.generateParagraphs(1)}</p>
+                <p>{desc.generateParagraphs(1)}</p>
+                <p>{desc.generateParagraphs(1)}</p>
             </div>
             <div class="content-container-right">
-                <p>Workload</p>
-                <p>Coordinators (with title)</p>
-                <p>ECTS</p>
-                <p>Schedule group</p>
-                <p>Block</p>
-                <p>Faculty</p>
-                <p>Level</p>
-                <p>Exam</p>
+                <div class="side-card"
+                    style="
+                        --bg-color: {theme.colors.neutral[800]};
+                        --text-color: {theme.colors.neutral[200]};
+                        --sub-title-color: {theme.colors.neutral[600]};
+                        --brand-color: {theme.colors.brand[500]};
+                        "
+                >
+                    <h3 class="side-card-heading">Coordinators</h3>
+                    <div class="coordinator-name-title">
+                        <p class="coordinator-name">Jon Sporring</p>
+                        <p class="coordinator-title">(Professor)</p>
+                    </div>
+                    <p class="coordinator-email">sporring@di.ku.dk</p>
+
+                    <div class="coordinator-name-title">
+                        <p class="coordinator-name">Josh Niemela</p>
+                        <p class="coordinator-title">(baller)</p>
+                    </div>
+                    <p class="coordinator-email">rick@roll.com</p>
+                </div>
+                <div class="side-card"
+                    style="
+                        --bg-color: {theme.colors.neutral[800]};
+                        --text-color: {theme.colors.neutral[200]};
+                        --sub-title-color: {theme.colors.neutral[600]};
+                        --brand-color: {theme.colors.brand[500]};
+                        "
+                >
+                    <h3 class="side-card-heading">Info</h3>
+                </div>
+                <div class="side-card"
+                    style="
+                        --bg-color: {theme.colors.neutral[800]};
+                        --text-color: {theme.colors.neutral[200]};
+                        --sub-title-color: {theme.colors.neutral[600]};
+                        --brand-color: {theme.colors.brand[500]};
+                        "
+                >
+                    <h3 class="side-card-heading">Schedule</h3>
+                </div>
+                <div class="side-card"
+                    style="
+                        --bg-color: {theme.colors.neutral[800]};
+                        --text-color: {theme.colors.neutral[200]};
+                        --sub-title-color: {theme.colors.neutral[600]};
+                        --brand-color: {theme.colors.brand[500]};
+                        "
+                >
+                    <h3 class="side-card-heading">Workload</h3>
+                </div>
+                <div class="side-card"
+                    style="
+                        --bg-color: {theme.colors.neutral[800]};
+                        --text-color: {theme.colors.neutral[200]};
+                        --sub-title-color: {theme.colors.neutral[600]};
+                        --brand-color: {theme.colors.brand[500]};
+                        "
+                >
+                    <h3 class="side-card-heading">Exam</h3>
+                </div>
             </div>
         </div>
     </div>
@@ -63,33 +138,72 @@ onMount(async () => {
     flex-direction: column;
     justify-content: start;
     align-items: center;
-    background-color: yellow;
 }
 
 .header-container {
     display: flex;
     width: 100%;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: start;
-    align-items: start;
+    align-items: center;
 }
 
 .content-container {
     height: 100%;
     width: 100%;
     display: grid;
-    grid-template: 1fr / 3fr 2fr;
+    grid-template: 1fr / 3fr 1fr;
 }
 
 .content-container-right {
-    height: 100%;
-    width: 100%;
-    background-color: red;
+    height: 92%;
+    width: 92%;
+    padding: 4%;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
 }
 
 .content-container-left {
-    height: 100%;
-    width: 100%;
-    background-color: grey;
+    height: 92%;
+    width: 92%;
+    padding: 4%;
 }
+
+.side-card {
+    width: 90%;
+    margin-bottom: 2vh;
+    background-color: var(--bg-color);
+    color: var(--text-color);
+    padding: 2%;
+    border-radius: 10px;
+}
+
+.side-card-heading {
+    font-size: 1.5rem;
+    color: var(--text-color);
+}
+.coordinator-name-title {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+}
+.coordinator-name {
+    font-size: 1rem;
+    color: var(--text-color);
+}
+
+.coordinator-title {
+    font-size: 1rem;
+    color: var(--sub-title-color);
+    margin-left: 0.5vw;
+}
+
+.coordinator-email {
+    font-size: 1rem;
+    color: var(--brand-color);
+    margin-bottom: 1vh;
+}
+
 </style>
