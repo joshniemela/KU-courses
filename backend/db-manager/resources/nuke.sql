8b4b0f36b2d172aedd4e07151509de76ea1900b8
Joshua Niemelä <josh@jniemela.dk>
2023-06-08 09:56:06 +0200

    formatted sqls

--- backend/db-manager/resources/nuke.sql
+++ backend/db-manager/resources/nuke.sql
@@ -1,8 +1,14 @@
 -- NUKE IT
 BEGIN;
-
-DROP TABLE IF EXISTS course, schedule, exam, title, coordinates, employee, workload;
-
-DROP TYPE IF EXISTS schedule_enum, block_enum, exam_enum, work_enum;
-
+DROP TABLE IF EXISTS course,
+schedule,
+exam,
+title,
+coordinates,
+employee,
+workload;
+DROP TYPE IF EXISTS schedule_enum,
+block_enum,
+exam_enum,
+work_enum;
 END;
--- backend/db-manager/resources/schema.sql
+++ backend/db-manager/resources/schema.sql
@@ -1,68 +1,54 @@
 -- Initialize database
 BEGIN;
-
 CREATE TABLE IF NOT EXISTS course (
 	course_id char(10) PRIMARY KEY,
-    url text NOT NULL,
+	url text NOT NULL,
 	title text NOT NULL,
 	course_language text,
 	description text NOT NULL,
 	raw_description text NOT NULL,
-	start_block block_enum, -- TODO: add not null (JAKUP HERE, we can't because some are missing)
+	start_block block_enum,
 	duration int,
 	credits numeric(3, 1),
 	study_level text
 );
-
 CREATE TABLE IF NOT EXISTS exam (
 	course_id char(10) NOT NULL,
 	exam_type exam_enum NOT NULL,
 	minutes int
 );
-
 CREATE TABLE IF NOT EXISTS employee (
 	email text PRIMARY KEY,
 	full_name text NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS title (
 	email text NOT NULL,
 	title text NOT NULL -- Potentially enum in the future
 );
-
 CREATE TABLE IF NOT EXISTS coordinates (
 	email text NOT NULL,
 	course_id char(10) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS workload (
 	course_id char(10) NOT NULL,
-  hours int,
+	hours int,
 	workload_type work_enum NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS schedule (
 	course_id char(10) NOT NULL,
 	schedule_type schedule_enum NOT NULL
 );
-
 -- Add constraints
 ALTER TABLE exam
-    ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
-	
+ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
 ALTER TABLE title
-	ADD CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES employee (email);
-	
+ADD CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES employee (email);
 ALTER TABLE coordinates
-	ADD CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES employee (email);
-	
+ADD CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES employee (email);
 ALTER TABLE coordinates
-	ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
-	
+ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
 ALTER TABLE workload
-	ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
-
+ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
 ALTER TABLE schedule
-	ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
-
+ADD CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course (course_id);
 END;
