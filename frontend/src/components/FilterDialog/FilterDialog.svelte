33855c3a3c54c5b680991d8f272bf24b63365574
Kristian <kristian.dam.pedersen@gmail.com>
2023-06-08 15:49:07 +0200

    fixed issue with filters not reflecting underlying state when reloaded

--- frontend/src/components/FilterDialog/FilterDialog.svelte
+++ frontend/src/components/FilterDialog/FilterDialog.svelte
@@ -14,10 +14,11 @@ export let dialog;
 /**
 * Helper function to generate option objects based on the different filter types
 */
-function generateOptionsObject(TypeObject) {
+function generateOptionsObject(TypeObject, field) {
     let obj = {};
-    for (const [_, val] of Object.entries(TypeObject)) {
-        obj[val] = false
+    for (const [key, val] of Object.entries(TypeObject)) {
+        console.log($filtersObj[field])
+        obj[val] = ($filtersObj[field].length > 0)
     }
     return obj
 }
@@ -35,25 +36,58 @@ function aggregateOptions(optionsObject) {
     return li
 }
 
+/**
+* Applies the new filters
+*/
 function applyOptions() {
     // Apply studyLevelOptions
     $filters = jsonToString({
         ...$filtersObj,
-        'study_level': aggregateOptions(studyLevelOptions)
+        'study_level': aggregateOptions(studyLevelOptions),
+        'scheduleGroupOptions': aggregateOptions(scheduleGroupOptions)
     })
 }
-let studyLevelOptions = generateOptionsObject(StudyLevelTypes)
+
+
+let studyLevelOptions = generateOptionsObject(StudyLevelTypes, 'study_level')
+let scheduleGroupOptions = generateOptionsObject(ScheduleGroupTypes, 'schedule_group')
 </script>
 
 <dialog bind:this={dialog} on:close>
+    <div class="filter-container">
     <button on:click={() => console.log(aggregateOptions(studyLevelOptions))}> log </button>
     <button on:click={applyOptions}>Apply</button>
+    <p>Course level</p>
     {#each Object.entries(StudyLevelTypes) as [key, value]}
-        <input
-            type="checkbox"
-            bind:checked={studyLevelOptions[value]}
-            id={key}
-        />
-        <label for={key}>{value}</label>
+        <div>
+            <input
+                type="checkbox"
+                bind:checked={studyLevelOptions[value]}
+                id={key}
+            />
+            <label for={key}>{value}</label>
+        </div>
     {/each}
+
+    <p>Schedule group</p>
+    {#each Object.entries(ScheduleGroupTypes) as [key, value]}
+        <div>
+            <input
+                type="checkbox"
+                bind:checked={scheduleGroupOptions[value]}
+                id={key}
+            />
+            <label for={key}>{value}</label>
+        </div>
+    {/each}
+    <p>Block</p>
+    <p>Exam type</p>
+    </div>
 </dialog>
+
+<style scoped>
+.filter-container {
+    display: flex;
+    flex-direction: column;
+}
+</style>
