b27cf052f23d3bd43f72b251cdbc128e999431ab
Kristian <kristian.dam.pedersen@gmail.com>
2023-06-10 12:13:59 +0200

    pit stop commit

--- frontend/src/components/FilterDialog/FilterDialog.svelte
+++ frontend/src/components/FilterDialog/FilterDialog.svelte
@@ -59,73 +59,115 @@ let scheduleGroupOptions = generateOptionsObject(ScheduleGroupTypes, 'schedule_g
 let blockOptions = generateOptionsObject(BlockTypes, 'block')
 let examTypeOptions = generateOptionsObject(ExamTypes, 'exam_type')
 
+/**
+    * Checks wether we are clicking on the background, and if so closes the dialog 
+    * @function closeOnClickOutside 
+*/
+function closeOnClickOutside(event) {
+        if (event.target.id == 'dialog') {
+            dialog.close()
+        }
+    }
 onMount(() => {
 })
 </script>
 
-<dialog bind:this={dialog} on:close={() => {
+<dialog class="dialog" bind:this={dialog}
+    id="dialog"
+    on:click={closeOnClickOutside}
+    on:close={() => {
     studyLevelOptions = generateOptionsObject(StudyLevelTypes, 'study_level')
     scheduleGroupOptions = generateOptionsObject(ScheduleGroupTypes, 'schedule_group')
     blockOptions = generateOptionsObject(BlockTypes, 'block')
     examTypeOptions = generateOptionsObject(ExamTypes, 'exam_type')
 }}>
     <div class="filter-container">
-    <button on:click={() => console.log($filtersObj)}> log </button>
-    <button on:click={applyOptions}>Apply</button>
-    <p>Course level</p>
-    {#each Object.entries(StudyLevelTypes) as [key, value]}
-        <div>
-            <input
-                type="checkbox"
-                bind:checked={studyLevelOptions[value]}
-                id={key}
-            />
-            <label for={key}>{value}</label>
+        <h1>Filters</h1>
+        <div class="filter-content-container">
+            <div>
+                <p>Course level</p>
+                {#each Object.entries(StudyLevelTypes) as [key, value]}
+                    <div>
+                        <input
+                            type="checkbox"
+                            bind:checked={studyLevelOptions[value]}
+                            id={key}
+                        />
+                        <label for={key}>{value}</label>
+                    </div>
+                {/each}
+            </div>
+            <div>
+                <p>Schedule group</p>
+                {#each Object.entries(ScheduleGroupTypes) as [key, value]}
+                    <div>
+                        <input
+                            type="checkbox"
+                            bind:checked={scheduleGroupOptions[value]}
+                            id={key}
+                        />
+                        <label for={key}>{value}</label>
+                    </div>
+                {/each}
+            </div>
+            <div>
+            <p>Block</p>
+                {#each Object.entries(BlockTypes) as [key, value]}
+                    <div>
+                        <input
+                            type="checkbox"
+                            bind:checked={blockOptions[value]}
+                            id={key}
+                        />
+                        <label for={key}>{value}</label>
+                    </div>
+                {/each}
+            </div>
+            <div>
+                <p>Exam type</p>
+                {#each Object.entries(ExamTypes) as [key, value]}
+                    <div>
+                        <input
+                            type="checkbox"
+                            bind:checked={examTypeOptions[value]}
+                            id={key}
+                        />
+                        <label for={key}>{convertExamToString(value)}</label>
+                    </div>
+                {/each}
+            </div>
         </div>
-    {/each}
-
-    <p>Schedule group</p>
-    {#each Object.entries(ScheduleGroupTypes) as [key, value]}
-        <div>
-            <input
-                type="checkbox"
-                bind:checked={scheduleGroupOptions[value]}
-                id={key}
-            />
-            <label for={key}>{value}</label>
-        </div>
-    {/each}
-
-    <p>Block</p>
-    {#each Object.entries(BlockTypes) as [key, value]}
-        <div>
-            <input
-                type="checkbox"
-                bind:checked={blockOptions[value]}
-                id={key}
-            />
-            <label for={key}>{value}</label>
-        </div>
-    {/each}
-    <p>Exam type</p>
-    {#each Object.entries(ExamTypes) as [key, value]}
-        <div>
-            <input
-                type="checkbox"
-                bind:checked={examTypeOptions[value]}
-                id={key}
-            />
-            <label for={key}>{convertExamToString(value)}</label>
-        </div>
-    {/each}
+        <button on:click={() => console.log($filtersObj)}> log </button>
+        <button on:click={applyOptions}>Apply</button>
     </div>
 </dialog>
 
 <style scoped>
+.dialog {
+    width:100%;
+    height:100%;
+    background-color:rgba(0,0,0,0.8);
+}
+
 .filter-container {
+    background-color:#ffffff;
+    width: 30%;
+    height: 60%;
+    overflow: scroll;
+    margin: auto;
+    padding: 1%;
+    margin-top: 15vh;
     display: flex;
     flex-direction: column;
-    height: 100%;
+    justify-content: start;
+    align-items: center;
+}
+
+.filter-content-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: yellow;
     width: 100%;
+    height: 100%;
 }
 </style>
