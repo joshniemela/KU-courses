8eb899ae7e1e2672b763e6054795575119476752
Joshua Niemelä <josh@jniemela.dk>
2023-06-15 01:01:35 +0200

    Clean-up

--- queries/courses_pr_employee.sql
+++
@@ -1,7 +0,0 @@
-WITH course_count AS (
-	SELECT count(email) as course_count, email FROM coordinates
-	GROUP BY email
-)
-SELECT full_name, employee.email, course_count FROM employee 
-JOIN course_count ON course_count.email = employee.email
-ORDER BY course_count DESC;
