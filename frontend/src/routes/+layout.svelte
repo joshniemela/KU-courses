9c66bf024a357db8fc33a3b97ada63ca4147a4fa
Joshua Niemelä <josh@jniemela.dk>
2023-07-15 14:57:29 +0200

    changed goatcounter

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Navbar from "../components/Navbar/Navbar.svelte";
     import "../app.css";
-
+    import { afterNavigate } from "$app/navigation";
     // <Navbar>
     //     <slot name="navbar" />
     // </Navbar>
@@ -10,10 +10,20 @@
 <svelte:head>
 
 <!-- analtyics -->
+    <script>
+        afterNavigate(({ to }) => {
+            if (window.goatcounter) {
+                window.goatcounter.count({
+                    path: to.path
+                });
+            }
+        };
+    </script>
 	<script 
 		async
 		src='//gc.zgo.at/count.js'
 		data-goatcounter='https://disku.goatcounter.com/count'
+        data-goatcounter-settings='{"allow_local": true}'
 	></script>
 </svelte:head>
 
