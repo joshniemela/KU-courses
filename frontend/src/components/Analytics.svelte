587630d8d0af6320d2e2d8ab0844b74cf2d7ba32
Joshua Niemelä <josh@jniemela.dk>
2023-07-15 14:05:36 +0200

    Added goatcounter

---
+++ frontend/src/components/Analytics.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+// Taken from dogatekin
+  import { onMount } from 'svelte';
+  import { afterNavigate } from '$app/navigation';
+
+  onMount(() => {
+    const count = document.createElement('script');
+    count.async = true;
+    count.src = "//gc.zgo.at/count.js"
+    count.setAttribute('data-goatcounter',`https://jniemela.goatcounter.com/count`);
+    count.setAttribute('data-goatcounter-settings',JSON.stringify({ no_onload: false }));
+    const body = document.getElementById('content');
+    body?.appendChild(count);
+  });
+
+  afterNavigate(({ to }) => {
+    // @ts-ignore
+    if(window.goatcounter) {
+      // @ts-ignore
+      window.goatcounter.count({
+        path: to.pathname,
+      })
+    }
+  });
+</script>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import Navbar from "../components/Navbar/Navbar.svelte";
     import "../app.css";
+    import Analytics from "../components/Analytics.svelte";
 
     // <Navbar>
     //     <slot name="navbar" />
     // </Navbar>
 </script>
+
+<Analytics />
 
 <div class="main">
     <div class="slot-container content-offset">
