6271a370f08edb04fe019a35c40866fb3d16c968
Joshua Niemelä <josh@jniemela.dk>
2023-07-15 15:52:53 +0200

    fixed

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,28 +2,27 @@
     import Navbar from "../components/Navbar/Navbar.svelte";
     import "../app.css";
     import { afterNavigate } from "$app/navigation";
+    import { browser } from "$app/environment";
+    import { page } from "$app/stores";
     // <Navbar>
     //     <slot name="navbar" />
     // </Navbar>
+
+    $: if (browser && window.goatcounter) {
+        window.goatcounter.count({
+          path: $page.url.pathname
+        });
+        console.log("counting", $page.url.pathname)
+      }
 </script>
 
 <svelte:head>
 
 <!-- analtyics -->
-    <script>
-        afterNavigate(({ to }) => {
-            if (window.goatcounter) {
-                window.goatcounter.count({
-                    path: to.path
-                });
-            }
-        };
-    </script>
 	<script 
-		async
-		src='//gc.zgo.at/count.js'
-		data-goatcounter='https://disku.goatcounter.com/count'
-        data-goatcounter-settings='{"allow_local": true}'
+    data-goatcounter-settings={'{"allow-local":true, "no_onload": true}'}
+		data-goatcounter="https://disku.goatcounter.com/count"
+		async src="//gc.zgo.at/count.js"
 	></script>
 </svelte:head>
 
