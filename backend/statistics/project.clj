852af34a1f7c0a591d3b65fe8194080e77d8a8ea
Joshua Niemelä <josh@jniemela.dk>
2023-07-30 16:54:40 +0200

    Added stuff

--- backend/statistics/project.clj
+++ backend/statistics/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+
+                 [org.clojure/data.json "2.4.0"]]
   :main ^:skip-aot statistics.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- backend/statistics/src/statistics/core.clj
+++ backend/statistics/src/statistics/core.clj
@@ -1,5 +1,33 @@
 (ns statistics.core
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io])
   (:gen-class))
+
+(def data-dir "../../data/")
+(def json-dir (str data-dir "json/"))
+
+; Read every json in data-dir
+(defn read-json [file]
+  (json/read-str (slurp (str json-dir file)) :key-fn keyword))
+
+; find all jsons
+(def course-files (for [file (file-seq (io/file json-dir)) :when (.endsWith (.getName file) ".json")]
+                    (.getName file)))
+
+(def courses (map read-json course-files))
+; each course has a key called "start_block", if it's 1 or 2 then it is winter,
+; if it's 3 or 4 then it is summer
+
+; HOW TO GENERATE THE COURSE STATISTICS PAGE URL:
+; start with base https://karakterstatistik.stads.ku.dk/Histogram/
+; add the course-id which also exists in each course map
+; the course ID has a "U" at the end, this has to be changed to an "E" for exams
+; add semester which is "Winter" or "Summer"
+; add year which is the year of the exam
+; EXAMPLE: Advanced Algorithms and Data Structures (AADS)
+; NDAA09023U - SCIENCE
+; =>
+; https://karakterstatistik.stads.ku.dk/Histogram/NDAA09023E/Winter-2022
 
 (defn -main
   "I don't do a whole lot ... yet."
