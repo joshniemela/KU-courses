6c3f5ac337692368c68d29e77670ece18d20cdef
Joshua Niemelä <josh@jniemela.dk>
2023-08-02 23:17:48 +0200

    broken file, but the actual statistics parser finally works

--- backend/statistics/src/statistics/utils.clj
+++ backend/statistics/src/statistics/utils.clj
@@ -1,5 +1,51 @@
 (ns statistics.utils
   (:require
-   [clojure.data.json :as json]
    [clojure.java.io :as io]
    [clojure.string :as str]))
+
+
+;  "exam": [
+;    {
+;      "grade": "12",
+;      "count": 13
+;    },
+;    {
+;      "grade": "10",
+;      "count": 24
+;    },
+;    {
+;      "grade": "7",
+;      "count": 30
+;    },
+;    {
+;      "grade": "4",
+;      "count": 22
+;    },
+;    {
+;      "grade": "02",
+;      "count": 13
+;    },
+;    {
+;      "grade": "00",
+;      "count": 11
+;    },
+;    {
+;      "grade": "-3",
+;      "count": 9
+;    },
+;    {
+;      "grade": "Failed",
+;      "count": 17
+;    }
+
+(def passing-grades ["Passed" "12" "10" "7" "4" "02"])
+(def failing-grades ["00" "-3" "Failed" "Absent"])
+
+(defn pass-fail-course? [grade]
+  (contains? grade "Passed"))
+
+(defn compute-stats [exam-map]
+  
+          
+
+
