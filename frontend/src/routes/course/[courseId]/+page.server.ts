aeddbdadc851af294370316d2595142ef3124e26
Joshua Niemelä <josh@jniemela.dk>
2023-08-03 10:01:16 +0200

    Pages are now generated serverside since they do not require the client

---
+++ frontend/src/routes/course/[courseId]/+page.server.ts
@@ -0,0 +1,26 @@
+import { apiUrl } from "../../../stores";
+import { total_hours } from "../../../course";
+
+
+export async function load({ fetch, params }) {
+    const { courseId } = params;
+    const API_URL = apiUrl();
+
+    const res = await fetch(`${API_URL}/get-course?id=${courseId}`, {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+
+    const course = await res.json();
+
+    return {
+        courseId: courseId,
+        course: course,
+        totalHours: total_hours(course),
+        loading: false,
+    };
+
+}
