859447ce680154b3c7672c58f9a5409a3b40c64f
Kristian <kristian.dam.pedersen@gmail.com>
2023-08-03 07:44:46 +0200

    Initial scaffolding

---
+++ frontend/src/routes/graph-example/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import GraphComponent from "../../components/GraphComponent/GraphComponent.svelte";
+</script>
+
+<div class="container">
+<h1> Graph example </h1>
+<GraphComponent />
+</div>
+
+
+<style scoped>
+.container {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
+
+
