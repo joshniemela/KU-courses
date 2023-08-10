402a4c83106a7113c80e68d73b501da5a3567e01
Joshua Niemelä <josh@jniemela.dk>
2023-08-10 13:45:03 +0200

    made sitemap generator

---
+++ frontend/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,58 @@
+// look in the data folder and get the names of all files in the jsons
+const jsons_dir = "../data/json/";
+const url = "https://disku.jniemela.dk";
+
+import * as fs from 'fs';
+function all_course_names(): string[] {
+    return fs.readdirSync(jsons_dir).map((x) => x.replace(".json", ""))
+}
+
+
+function today_yyyy_mm_dd(): string {
+    const d = new Date();
+    const iso = d.toISOString();
+    return iso.substring(0, 10);
+}
+
+function generate_xml(course_name: string): string {
+    return `
+    <url>
+        <loc>${url}/course/${course_name}</loc>
+        <priority>0.8</priority>
+        <lastmod>${today_yyyy_mm_dd()}</lastmod>
+    </url>
+    `
+}
+
+
+
+export async function GET() {
+    return new Response(
+        `
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xhtml="https://www.w3.org/1999/xhtml"
+      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+    >
+
+      <!-- root -->
+       <url>
+        <loc>${url}</loc>
+        <lastmod>${today_yyyy_mm_dd()}</lastmod>
+        <priority>1.0</priority>
+      </url>
+      <!-- course pages -->
+      ${all_course_names().map(generate_xml).join("\n")}
+
+    </urlset>`.trim(),
+        {
+            headers: {
+                "Content-Type": "application/xml",
+            },
+        }
+    )
+}
