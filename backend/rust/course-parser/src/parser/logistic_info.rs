59549c99eeac15c5997e09a20bb47850db473c4e
Kristian <kristian.dam.pedersen@gmail.com>
2023-08-25 18:25:04 +0200

    Currently working on extracting rough information from the html

--- backend/rust/course-parser/src/parser/logistic_info.rs
+++ backend/rust/course-parser/src/parser/logistic_info.rs
@@ -22,41 +22,59 @@ pub fn parse(dom: &VDom) -> Result<parser::LogisticInformation, Box<dyn std::err
     })
 }
 
-fn extract_information(dom: &VDom) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
-    // // find all div class="panel-body" elements and assert that there is only one
-    // let panel_bodies = dom.get_elements_by_class_name("panel-body");
-    // let parser = dom.parser();
-    //
-    // // there might be multiple panel-bodies, so we need to check each one
-    // // for the dl element (only the course info should have a dl element)
-    // for (_i, panel_body) in panel_bodies.enumerate() {
-    //     let mut dl_elements = panel_body
-    //         .get(parser)
-    //         .ok_or("Failed to get panel-body")?
-    //         .as_tag()
-    //         .ok_or("Failed to get panel-body as tag")?
-    //         .query_selector(parser, "dl")
-    //         .ok_or("Failed to get dl from panel-body")?;
-    //     match dl_elements.next() {
-    //         Some(handle) => {
-    //             let node = handle
-    //                 .get(parser)
-    //                 .ok_or("Failed to get node")?
-    //                 .as_tag()
-    //                 .ok_or("Failed to get node as tag")?;
-    //             // parse DL
-    //             let course_infos = parse_dl(node, parser)?;
-    //             //println!("{course_infos:?}");
-    //             // parse the course information
-    //             let coerced_course_info = coerce_course_info(&course_infos, dom);
-    //             return coerced_course_info;
-    //         }
-    //         None => continue,
-    //     }
-    // }
-    // Err("No dl element found in the panel-body".into())
 
-    Ok(vec!((String::from("Test"), String::from("Value"))))
+fn extract_information(dom: &VDom) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
+    let parser = dom.parser();
+
+    // Find all div class="panel-body" elements
+    let panel_bodies = dom
+        .get_elements_by_class_name("panel-body")
+        .next()
+        .unwrap()
+        .get(parser)
+        .unwrap()
+        .as_tag()
+        .unwrap();
+
+    let mut dept_heading: Option<String> = None;
+    let mut faculty_heading: Option<String> = None;
+    let mut coordinators_heading: Option<Vec<String>> = None;
+
+    let mut dept: Option<String> = None;
+    let mut faculty: Option<String> = None;
+    let mut coordinators: Option<Vec<String>> = None;
+
+    // Iterate over h5's
+    let h5 = panel_bodies.query_selector(parser, "h5").unwrap();
+    h5.for_each(|h5_node| {
+            let html_inner = h5_node.get(parser).unwrap().inner_text(parser);
+            println!("{:?}", &html_inner);
+            match &html_inner {
+                x if x.contains("Department") => dept = Some(html_inner.to_string()),
+                _ => ()
+            }
+    });
+
+    // Iterate over the lists
+    let mut lists: Vec<String> = Vec::new();
+    let uls = panel_bodies.query_selector(parser, "ul").unwrap();
+    uls.for_each(|ul| {
+        let li = ul.get(parser).unwrap().as_tag().unwrap().inner_text(parser);
+        println!("Content: {}", li);
+        match &li.to_lowercase() {
+            x if x.contains("department") => dept_heading = Some(li.to_string()),
+            _ => ()
+        }
+    });
+
+    let mut res: Vec<(String, String)> = Vec::new();
+
+    if dept_heading.is_some() && dept.is_some() {
+        res.push((dept_heading.unwrap(), dept.unwrap()));
+    }
+
+
+    Ok(res)
 }
 
 fn parse_contracting_departments(input: &str) -> Result<Vec<parser::Department>, Box<dyn std::error::Error>> {
