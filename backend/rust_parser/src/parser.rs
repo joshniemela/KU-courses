7e84a1672cb0013a7837d6ec39f7f1d5ab85495c
Joshua Niemelä <josh@jniemela.dk>
2023-10-04 16:13:05 +0200

    Using anyhow

--- backend/rust_parser/src/parser.rs
+++ backend/rust_parser/src/parser.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use anyhow::{anyhow, ensure, Result};
 use tl::VDom;
 
 #[derive(Debug, PartialEq)]
@@ -6,22 +6,20 @@ pub struct Course {
     pub title: String,
 }
 
-pub fn parse_course(html: &str) -> Result<Course, Box<dyn Error>> {
+pub fn parse_course(html: &str) -> Result<Course> {
     let dom = tl::parse(html, tl::ParserOptions::default())?;
     let _content = dom.get_element_by_id("content");
     let title = parse_title(&dom)?;
     Ok(Course { title })
 }
-fn parse_title(dom: &VDom) -> Result<String, Box<dyn std::error::Error>> {
+fn parse_title(dom: &VDom) -> Result<String> {
     let title = dom
         .get_elements_by_class_name("courseTitle")
         .next()
-        .ok_or_else::<Box<dyn std::error::Error>, _>(|| {
-            "Unable to grab course title from dom".into()
-        })
+        .ok_or_else(|| anyhow!("Unable to find course title"))
         .and_then(|elem| {
             elem.get(dom.parser())
-                .ok_or_else(|| "Unable to grab parser for the dom, this should not happen".into())
+                .ok_or_else(|| anyhow!("Unable to grab parser for the dom, this should not happen"))
                 .map(|tag| tag.inner_text(dom.parser()))
         });
 
@@ -33,9 +31,10 @@ fn parse_title(dom: &VDom) -> Result<String, Box<dyn std::error::Error>> {
     let res: Vec<&str> = binding.split_whitespace().collect();
 
     // Return only the part of the title without the course code
-    if res.len() >= 2 {
-        Ok(res[1..].join(" "))
-    } else {
-        Err("Title does not conform to expected structure: <COURSECODE> <NAME>".into())
-    }
+    ensure!(
+        res.len() >= 2,
+        "Title does not conform to expected structure: <COURSECODE> <NAME>"
+    );
+
+    Ok(res[1..].join(" "))
 }
