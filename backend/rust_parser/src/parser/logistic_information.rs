f8a8984a8c258af650773156d5b91944fe974436
Joshua Niemelä <josh@jniemela.dk>
2023-10-05 11:42:38 +0200

    adding logistic info parser from Kristian

---
+++ backend/rust_parser/src/parser/logistic_information.rs
@@ -0,0 +1,8 @@
+use crate::parser;
+use crate::parser::LogisticInformation;
+use anyhow::{anyhow, bail, ensure, Context, Result};
+use tl::VDom;
+
+pub fn parse_logistic_info(dom: &VDom) -> Result<LogisticInformation> {
+    unimplemented!()
+}
