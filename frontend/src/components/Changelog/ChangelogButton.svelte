90f012390c5a73ba25e4e9dad23061e761e3990b
Miguel Magueijo <miguelmagueijo@hotmail.com>
2024-08-17 21:08:20 +0100

    increase open changelog btn on smaller screens

--- frontend/src/components/Changelog/ChangelogButton.svelte
+++ frontend/src/components/Changelog/ChangelogButton.svelte
@@ -2,6 +2,6 @@
     import { modalStore } from "./store";
 </script>
 
-<button type="button" class="p-2 text-kuRed/75 text-sm" on:click={modalStore.open}>
+<button type="button" class="p-2 text-kuRed/75 underline md:no-underline md:text-sm" on:click={modalStore.open}>
     View Changelog
 </button>
