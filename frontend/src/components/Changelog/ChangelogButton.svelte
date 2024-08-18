765250c432ce3c94c4e5e9c1abb7045cd11ef33c
Miguel Magueijo <miguelmagueijo@hotmail.com>
2024-08-18 15:47:35 +0100

    remove animation from button

--- frontend/src/components/Changelog/ChangelogButton.svelte
+++ frontend/src/components/Changelog/ChangelogButton.svelte
@@ -3,25 +3,5 @@
 </script>
 
 <button type="button" class="text-kuRed/75 underline md:no-underline md:text-sm relative" on:click={modalStore.open}>
-    <span class="p-2">
-        View Changelog
-    </span>
-
-    <span class="absolute block h-full w-full bg-kuRed top-0 left-0 -z-10">
-    </span>
+    View Changelog
 </button>
-
-<style>
-    button span {
-        height: 0%;
-        transition-duration: 300ms;
-    }
-
-    button:hover span {
-        height: 100%;
-    }
-
-    button:hover {
-        color: white;
-    }
-</style>
